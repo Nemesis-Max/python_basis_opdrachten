4a953d5447f210b3962596325724daa425423f75
Nemesis-Max <97031008@st.deltion.nl>
2025-02-14 15:39:13 +0100

    Concept versie 0.2

--- opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
+++ opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
@@ -1,6 +1,6 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Rick van de Mars
+# Groep: 97031008
 
 # Hier komt je code...
 
@@ -8,7 +8,9 @@
 # Zorg dat de volgende zin op het scherm wordt getoond met de print-functie:
 # O mamma mia, wat maak je toch heerlijke pizza's
 
-print()
+a1 = "O mamma mia, wat maak je toch heerlijke pizza's"
+
+print(a1)
 
 # Gebruik onderstaande variabelen en de print-functie
 # en toon de volgende zin op het scherm: Albert Einstein zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!'
@@ -16,4 +18,7 @@ voornaam = "albert"
 achternaam = "einstein"
 
 
-print()
+b1 = {"voornaam": "Albert", "achternaam": "Einstein"}
+Tekst = "zei ooit: \'Als je niks fout doet, dan kun je ook niks leren!\'"
+
+print(b1 + Tekst)
