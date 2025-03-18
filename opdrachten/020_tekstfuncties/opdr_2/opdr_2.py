2f7b3978037af56aa33704b0e7458213277915d0
Nemesis-Max <97031008@st.deltion.nl>
2025-03-18 14:24:05 +0100

    ingevulde opdracht

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -4,6 +4,8 @@
 
 # Hier komt je code...
 
-my_list = ...
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
 
-print()
+t_count = my_string.lower().count('t')
+
+print(f"De letter 't' komt {t_count}x voor in my_string")
