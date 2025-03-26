c1c06219ea141b0e4463ed1d71d18d40786f65cd
Nemesis-Max <97031008@st.deltion.nl>
2025-03-26 14:56:48 +0100

    voltooide opdrachten

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,20 @@
 # Groep: 97031008
 
 # Hier komt je code...
+
+joe = 23
+bob = 15
+fred = 22
+
+totaal = joe + bob + fred
+
+gemiddelde = totaal / 3
+
+print(f"Dit weten we over joe, bob en fred:")
+print(f"De totale waarde van alle leeftijden bij elkaar opgeteld is {totaal}")
+print(f"De gemiddelde leeftijd van deze boys is {gemiddelde}")
+
+
+
+
+
